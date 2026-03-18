4722238fbfe48dc30176f909993fcf51c3eea888
wenonezh <VADIM.P.11111@gmail.com>
2026-03-18 15:19:59 +0300

    Добавил getDayOfWeek и обновил шапку сайта

--- sites/Vadim/index.php
+++ sites/Vadim/index.php
@@ -1,16 +1,21 @@
 <?php
-require_once '../../tools/Tools.php';
+require_once '.././../tools/Tools.php';
 
 $tools = new Tools();
+$day = getdate();
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <title>Сайт Вадима</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
     <h1>Добро пожаловать, Вадим!</h1>
     <p><?= $tools->getHello() ?></p>
+    <p><?= $tools->getCurrentTime() ?></p>
+    <p><?= $tools->getDayOfWeek($day) ?></p>
 </body>
 </html>
--- tools/Tools.php
+++ tools/Tools.php
@@ -11,4 +11,36 @@ class Tools
     {
         return "Текущее время: " . date('H:i:s');
     }
+    
+    public function getDayOfWeek(array $day): string
+    {
+        $out = 'Ошибка: функция не получила функцию `getdate()`';
+        
+        switch ($day['weekday']) {
+            case 'Sunday':
+                $out = 'Сегодня воскресенье! отдыхаем перед понедельником!';
+                break;
+            case 'Monday':
+                $out = 'Буя, сегодня понедельник :)';
+                break;
+            case 'Tuesday':
+                $out = 'Вторник сегодня, ждем среды';
+                break;
+            case 'Wednesday':
+                $out = 'Сегодня среда ребятушки😊';
+                break;
+            case 'Thursday':
+                $out = 'Сегодня четверг, держимся';
+                break;
+            case 'Friday':
+                $out = 'Сегодня пятница, еще чуть-чуть!';
+                break;
+            case 'Saturday':
+                $out = 'Отдыхаем! Сегодня суббота!';
+                break;
+        }
+        
+        $out = '<br>' . $out;
+        return $out;
+    }
 }
