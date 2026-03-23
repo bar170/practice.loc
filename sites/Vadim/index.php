8ee95fc8e45a9c582b52b6e6982a8c7fdaedcb6b
Grigoriev.E <grigorievegor.ru@gmail.com>
2026-03-23 11:06:29 +0300

    задачи на 23.03

--- sites/Vadim/index.php
+++ sites/Vadim/index.php
@@ -9,11 +9,16 @@ $tools = new Tools();
 <head>
     <meta charset="UTF-8">
     <title>Сайт Вадима</title>
+<!--    Где верстка?
+надо было восстановить состояние. Если по гиту сложно отследить, то просто на примере
+Даниила или Влада <- Папки рядом с твоей
+-->
 </head>
 <body>
     <h1>Добро пожаловать, Вадим!</h1>
-    <p><?= $tools->getHello() ?></p>
+    <p><?= $tools->getHello('Где тут твое имя?') ?></p>
     <p><?= $tools->getCurrentTime() ?></p>
-    <p><?= $tools->getDayOfWeek(getdate()) ?></p>
+    <p><?= $tools->getDayOfWeek(getdate()) ?></p> <!-- Ошибку же выдает, потому что функции не существует.
+   Потому что не правильный же require_once '../../common/Tools.php' не на тот Tools указывает-->
 </body>
 </html>
